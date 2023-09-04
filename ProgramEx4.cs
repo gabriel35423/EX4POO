8b20b22b8e28522c49543fed43fd0dc3b26ed559
gabriel35423 <137359307+gabriel35423@users.noreply.github.com>
2023-09-04 19:17:33 -0300

    Add files via upload

---
+++ ProgramEx4.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOOx4
+{
+    internal class Triangulo
+    {
+        private double b;
+        private double h;
+        private double a;
+
+
+        public void setb(double l)
+        {
+            b = l;
+
+        }
+        public void seth(double l)
+        {
+            h = l;
+
+        }
+        public double geta()
+        {
+            return a;
+        }
+        public double getb()
+        {
+            return b;
+        }
+
+        public double geth()
+        {
+            return h;
+        }
+
+        public void calcular()
+        {
+           a  = b * h / 2;
+        }
+    }
+}
